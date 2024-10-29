08538db7c64bbdd7acbc566fc6704b0372099e27
developerMasum <developer.masum2001@gmail.com>
2024-10-29 13:26:15 +0600

    added footer

--- client/app/(root)/page.tsx
+++ client/app/(root)/page.tsx
@@ -1,28 +1,36 @@
 import About from '@/components/NewHome/About';
+import BelowFooter from '@/components/NewHome/BelowFooter';
 import Categories from '@/components/NewHome/Categories';
+import JoinUs from '@/components/NewHome/JoinUs';
 import LatestPosts from '@/components/NewHome/LatestPosts';
 import CardsGrid from '@/components/NewHome/NewBlogCards';
 import NewsShelter from '@/components/NewHome/NewsShelter';
 import SocialFollow from '@/components/NewHome/SocialFollow';
+import TopNavbar from '@/components/NewHome/TopNavbar';
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center gap-12">
-      <div className="flex justify-start items-start gap-6">
-        {/* Main blog content area */}
-        <div>
-          <CardsGrid />
-        </div>
+    <main>
+      <div className="flex justify-center items-center gap-12">
+        <div className="flex justify-start items-start gap-6">
+          {/* Main blog content area */}
+          <div>
+            <CardsGrid />
+          </div>
 
-        {/* Sidebar with fixed positioning */}
-        <div>
-          <About />
-          <SocialFollow />
-          <NewsShelter />
-          <LatestPosts />
-          <Categories />
+          {/* Sidebar with fixed positioning */}
+          <div>
+            <TopNavbar />
+            <About />
+            <SocialFollow />
+            <NewsShelter />
+            <LatestPosts />
+            <Categories />
+            <JoinUs />
+          </div>
         </div>
       </div>
+      <BelowFooter />
     </main>
   );
 }
